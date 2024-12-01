daac12ffc880b55dfd06901f21509e58bb0d9990
B1n_ry <b1nry.dev@gmail.com>
2024-12-01 03:42:19 +0100

    Changed where all days are called from This makes it slightly easier to automate file generation Kinda useless since all files already existed, but I was bored ¯\_(ツ)_/¯

--- src/days/mod.rs
+++ src/days/mod.rs
@@ -23,3 +23,36 @@ pub mod day_22;
 pub mod day_23;
 pub mod day_24;
 pub mod day_25;
+ 
+pub fn run_one(file_input: &str, day: u16) {
+    match day {
+        1 => day_1::run(file_input),
+        2 => day_2::run(file_input),
+        3 => day_3::run(file_input),
+        4 => day_4::run(file_input),
+        5 => day_5::run(file_input),
+        6 => day_6::run(file_input),
+        7 => day_7::run(file_input),
+        8 => day_8::run(file_input),
+        9 => day_9::run(file_input),
+        10 => day_10::run(file_input),
+        11 => day_11::run(file_input),
+        12 => day_12::run(file_input),
+        13 => day_13::run(file_input),
+        14 => day_14::run(file_input),
+        15 => day_15::run(file_input),
+        16 => day_16::run(file_input),
+        17 => day_17::run(file_input),
+        18 => day_18::run(file_input),
+        19 => day_19::run(file_input),
+        20 => day_20::run(file_input),
+        21 => day_21::run(file_input),
+        22 => day_22::run(file_input),
+        23 => day_23::run(file_input),
+        24 => day_24::run(file_input),
+        25 => day_25::run(file_input),
+
+        0 => panic!("There is no 0th of December!"),
+        _ => panic!("Christmas has passed :("),
+    };
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 use std::{fs::File, io::Read, path::Path, time::Instant};
 
-use days::*;
-
 mod days;
 
 const DAY: u16 = 1;
@@ -24,37 +22,7 @@ fn main() {
 
     let now = Instant::now();
 
-    // Functions and arguments start with an underscore to not give compiler warnings
-    match DAY {
-        1  =>  day_1::run(&file_string),
-        2  =>  day_2::run(&file_string),
-        3  =>  day_3::run(&file_string),
-        4  =>  day_4::run(&file_string),
-        5  =>  day_5::run(&file_string),
-        6  =>  day_6::run(&file_string),
-        7  =>  day_7::run(&file_string),
-        8  =>  day_8::run(&file_string),
-        9  =>  day_9::run(&file_string),
-        10 => day_10::run(&file_string),
-        11 => day_11::run(&file_string),
-        12 => day_12::run(&file_string),
-        13 => day_13::run(&file_string),
-        14 => day_14::run(&file_string),
-        15 => day_15::run(&file_string),
-        16 => day_16::run(&file_string),
-        17 => day_17::run(&file_string),
-        18 => day_18::run(&file_string),
-        19 => day_19::run(&file_string),
-        20 => day_20::run(&file_string),
-        21 => day_21::run(&file_string),
-        22 => day_22::run(&file_string),
-        23 => day_23::run(&file_string),
-        24 => day_24::run(&file_string),
-        25 => day_25::run(&file_string),
-
-        0 => panic!("There is no 0th of December!"),
-        _ => panic!("Christmas has passed :(")
-    }
+    days::run_one(&file_string, DAY);
 
     if TIMER_ACTIVE {
         let elapsed = now.elapsed();
